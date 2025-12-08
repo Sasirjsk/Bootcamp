a008ca2921c273c3bc4577be45c8bbb74beb1173
Sasirjsk <sasirjsk27@gmail.com>
2025-12-08 09:56:31 -0500

    Update storage account name in backend.tf

--- azurebcproject2/env/stage/backend.tf
+++ azurebcproject2/env/stage/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "tfstate-rg"
-    storage_account_name = "tfstatesacct0t123"
+    storage_account_name = "tfstatesacct0t1234"
     container_name       = "tfstate"
     key                  = "aks/staging.tfstate"
   }
